5b6b3af3ea92540a7d1cde9d26262c6b07c5c3fb
“mariia.zolotarova” <zolotarovamariia16@gmail.com>
2024-04-28 16:38:37 +0300

    add env

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {ConfigProvider} from "antd";
 // http://localhost:1337/graphql
 
 const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
+    uri: `${process.env.BASE_URL}/graphql`,
     cache: new InMemoryCache(),
 });
 
