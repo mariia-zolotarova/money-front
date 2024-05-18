c7f8ee635adb49949dd4ddf88bb4cc7d72c4c635
“mariia.zolotarova” <zolotarovamariia16@gmail.com>
2024-05-18 13:54:58 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {Route, Routes} from "react-router-dom";
 import Categories from "./pages/Categories/Categories";
 import Charts from "./pages/Charts/Charts";
 import History from "./pages/History/History";
-import Person from "./pages/Person/Person";
 import Footer from "./components/Footer/Footer";
 import Registration from "./pages/Registration/Registration";
 import Authorization from "./pages/Authorization/Authorization";
@@ -21,7 +20,6 @@ export default function App() {
                     <Route path='/categories' element={<Categories/>}></Route>
                     <Route path='/charts' element={<Charts/>}></Route>
                     <Route path='/history' element={<History/>}></Route>
-                    <Route path='/person' element={<Person/>}></Route>
                     <Route path='/registration' element={<Registration/>}></Route>
                     <Route path='/authorization' element={<Authorization/>}></Route>
                     <Route path='/converter' element={<CurrencyConverter/>}></Route>
