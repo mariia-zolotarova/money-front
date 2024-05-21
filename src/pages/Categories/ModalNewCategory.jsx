7ee1b85f28573075c2f0e1b95f89f2a0a599df13
“mariia.zolotarova” <zolotarovamariia16@gmail.com>
2024-05-21 11:14:25 +0300

    fix validation category

--- src/pages/Categories/ModalNewCategory.jsx
+++ src/pages/Categories/ModalNewCategory.jsx
@@ -4,20 +4,35 @@ import {Input} from 'antd';
 
 export default function ModalNewCategory({setIsModalOpen, isModalOpen, newCategories}){
 
-    const [inputValue, setInputValue] = useState("Category Name");
+    const [inputValue, setInputValue] = useState();
+    const [isInputEmpty, setIsInputEmpty] = useState();
 
     const handleOk = () => {
+        if(inputValue){
         newCategories(inputValue);
         setIsModalOpen(false);
+        setInputValue("");
+        }
+        else{
+            setIsInputEmpty(true)
+        }
     };
     const handleCancel = () => {
         setIsModalOpen(false);
     };
 
+    const onInputChange = (event)=>{
+        setIsInputEmpty(false);
+        setInputValue(event.target.value);
+    }
+
     return(
         <>
             <Modal title="Add category" open={isModalOpen}  onCancel={handleCancel} onOk={handleOk}>
-                <Input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+                <Input value={inputValue} placeholder="Category Name" onChange={onInputChange} />
+                {
+                    isInputEmpty && <div className="authorization__error">❗️Enter Category Name</div>
+                }
             </Modal>
         </>
     )
